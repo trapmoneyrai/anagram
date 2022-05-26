8f8cc479fc0cc2b664777d656a2d6d2f72c7f854
TIP <97180669+trapmoneyrai@users.noreply.github.com>
2022-05-26 20:16:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if len(word) != len(anagram):
+        return False
+
+    return sorted(word) == sorted(anagram)
+
+print (find_anagram("hello", "check"))
+print (find_anagram("below", "elbow"))
